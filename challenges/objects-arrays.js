69fa21dd15ed08c7ae05d9ee1cd47f2c7c29e5cd
asupriana <alexa9410@gmail.com>
2019-02-01 12:47:15 -0500

    Update objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -167,24 +167,77 @@ console.log(uni);
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
-
-zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
-{"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
-{"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
-{"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
-{"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
-{"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
-{"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
-{"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
-{"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+zooAnimals = [{
+  "animal_name": "Jackal, asiatic",
+  "population": 5,
+  "scientific_name": "Canis aureus",
+  "state": "Kentucky"
+ },
+ {
+  "animal_name": "Screamer, southern",
+  "population": 1,
+  "scientific_name": "Chauna torquata",
+  "state": "Alabama"
+ },
+ {
+  "animal_name": "White spoonbill",
+  "population": 8,
+  "scientific_name": "Platalea leucordia",
+  "state": "Georgia"
+ },
+ {
+  "animal_name": "White-cheeked pintail",
+  "population": 1,
+  "scientific_name": "Anas bahamensis",
+  "state": "Oregon"
+ },
+ {
+  "animal_name": "Black-backed jackal",
+  "population": 2,
+  "scientific_name": "Canis mesomelas",
+  "state": "Washington"
+ },
+ {
+  "animal_name": "Brolga crane",
+  "population": 9,
+  "scientific_name": "Grus rubicundus",
+  "state": "New Mexico"
+ },
+ {
+  "animal_name": "Common melba finch",
+  "population": 5,
+  "scientific_name": "Pytilia melba",
+  "state": "Pennsylvania"
+ },
+ {
+  "animal_name": "Pampa gray fox",
+  "population": 10,
+  "scientific_name": "Pseudalopex gymnocercus",
+  "state": "Connecticut"
+ },
+ {
+  "animal_name": "Hawk-eagle, crowned",
+  "population": 10,
+  "scientific_name": "Spizaetus coronatus",
+  "state": "Florida"
+ },
+ {
+  "animal_name": "Australian pelican",
+  "population": 5,
+  "scientific_name": "Pelecanus conspicillatus",
+  "state": "West Virginia"
+ }
+];
 
 /* Request 1: .forEach()
 
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+const animalNames = []; 
+zooAnimals.forEach((animal) => {
+ animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}.`)
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -194,6 +247,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
+zooAnimals.map((animal) => lowerCase.push(animal.animal_name.toLowerCase())); 
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -202,6 +256,11 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const largerPopulation = [];
+zooAnimals.filter((animal) => {
+  if (animal.population < 5) {
+   largerPopulation.push(animal.animal_name);
+  }
+ }); 
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -209,7 +268,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((acc, total) => {
+  return acc += total.population;
+ }, 0); 
 console.log(populationTotal);
 
 
